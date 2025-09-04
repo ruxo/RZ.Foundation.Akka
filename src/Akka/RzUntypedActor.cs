50a0cfcff0cd1e32b810e7f106c0012a5feb35a1
Ruxo Zheng <ruxo@users.noreply.github.com>
2025-09-05 06:03:32 +0700

    Introduce RzUntypedActor

---
+++ src/Akka/RzUntypedActor.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.CompilerServices;
+using Akka.Actor;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace RZ.Foundation.Akka;
+
+[PublicAPI]
+public abstract class RzUntypedActor<T>(IServiceProvider sp) : UntypedActor where T : RzUntypedActor<T>
+{
+    protected readonly ILogger Logger = sp.GetRequiredService<ILogger<T>>();
+
+    /// <summary>
+    /// Drain all messages and stop the actor.
+    /// </summary>
+    protected void FloodAndStop() {
+        Become(OnFinalization);
+        Self.Tell(PoisonPill.Instance);
+    }
+
+    /// <summary>
+    /// Log with class and method name.
+    /// </summary>
+    /// <param name="message">The unhandled message</param>
+    /// <param name="caller">The caller's method name</param>
+    protected void LogUnhandled(object message, [CallerMemberName] string caller = "") {
+        Logger.LogWarning("{Processor}:[{Path}] cannot handle message from sender:[{Sender}]: {@Message}", caller, Self.Path.Name, Sender.Path.Name, message);
+    }
+
+    void OnFinalization(object message) {
+        Logger.LogDebug("Draining message: {@Message}", message);
+    }
+}
