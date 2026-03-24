54ac6d6a71ec3b394d282a276a7ce531f77a35ed
Ruxo Zheng <ruxo@users.noreply.github.com>
2026-03-24 08:35:32 +0700

    Introduce TraceableCommand

---
+++ src/Akka/TraceableCommand.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Annotations;
+using RZ.Foundation.Types;
+
+namespace RZ.Foundation.Akka;
+
+[PublicAPI]
+public record TraceableCommand<T>(ActivityId? ActivityId) where T: TraceableCommand<T>
+{
+    public T SwapActivity(ActivityId? activityId) => (T) this with { ActivityId = activityId };
+}
