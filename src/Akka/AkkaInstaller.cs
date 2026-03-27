11a9897dde4d2271d3a1e399372d04b5713dc7e4
Ruxo Zheng <ruxo@users.noreply.github.com>
2026-03-27 09:58:24 +0700

    Introduce Actor registration during initialization

--- src/Akka/AkkaInstaller.cs
+++ src/Akka/AkkaInstaller.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Frozen;
+using System.Runtime.CompilerServices;
 using Akka.Actor;
 using Akka.Actor.Setup;
 using Akka.DependencyInjection;
@@ -7,6 +8,20 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace RZ.Foundation.Akka;
 
+public delegate Props ActorPropsFactory(IServiceProvider sp);
+public delegate IActorRef ActorFactory(IServiceProvider sp, ActorSystem system);
+
+public sealed record ActorRegistration(string Name, ActorPropsFactory? PropsFactory = null, ActorFactory? ActorFactory = null)
+{
+    public bool IsValid => PropsFactory is not null || ActorFactory is not null;
+}
+
+[PublicAPI]
+public interface IAKkaServices
+{
+    IActorRef? GetService(string name);
+}
+
 [PublicAPI]
 public static class AkkaInstaller
 {
@@ -14,32 +29,45 @@ public static class AkkaInstaller
 
     extension(IServiceCollection services)
     {
+        [PublicAPI, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public IServiceCollection AddAkkaSystem(string systemName, params ActorRegistration[] registrations)
+            => services.AddAkkaSystem(systemName, DefaultAskTimeout, registrations);
+
         [PublicAPI]
-        public IServiceCollection AddAkkaSystem(string systemName, int defaultAskTimeout = DefaultAskTimeout) {
+        public IServiceCollection AddAkkaSystem(string systemName, int defaultAskTimeout, params ActorRegistration[] registrations) {
             if (defaultAskTimeout < 1) throw new ArgumentOutOfRangeException(nameof(defaultAskTimeout));
-            return services.AddAkkaSystem(systemName, string.Format(AkkaConfig.Simple, defaultAskTimeout));
+            return services.AddAkkaSystem(systemName, string.Format(AkkaConfig.Simple, defaultAskTimeout), registrations);
         }
 
         [PublicAPI]
-        public IServiceCollection AddAkkaSystem(string systemName, string hocon)
-            => services.AddAkkaSystem(systemName, [AkkaConfig.Bootstrap(hocon)]);
+        public IServiceCollection AddAkkaSystem(string systemName, string hocon, params ActorRegistration[] registrations)
+            => services.AddAkkaSystem(systemName, [AkkaConfig.Bootstrap(hocon)], registrations);
 
         [PublicAPI]
-        public IServiceCollection AddAkkaSystem(string systemName, Setup[] configs) {
+        public IServiceCollection AddAkkaSystem(string systemName, Setup[] configs, params ActorRegistration[] registrations) {
             if (string.IsNullOrWhiteSpace(systemName)) throw new ArgumentException(nameof(systemName));
 
-            services.AddSingleton(sp => {
-                var diSetup = DependencyResolverSetup.Create(sp);
-                var setup = ActorSystemSetup.Create(configs.Append(diSetup).ToArray());
-                return ActorSystem.Create(systemName, setup);
-            });
+            var lookup = new Impl.AkkaServices();
+            services.AddSingleton<IAKkaServices>(lookup)
+                    .AddSingleton(sp => {
+                         var diSetup = DependencyResolverSetup.Create(sp);
+                         var setup = ActorSystemSetup.Create(configs.Append(diSetup).ToArray());
+                         var system = ActorSystem.Create(systemName, setup);
+                         var dict = new Dictionary<string, IActorRef>();
+                         foreach (var (name, pFactory, aFactory) in registrations.Where(r => r.IsValid))
+                                 dict[name] = aFactory is null
+                                                  ? system.ActorOf(pFactory!(sp), name)
+                                                  : aFactory(sp, system);
+                         lookup.Actors = dict.ToFrozenDictionary();
+                         return system;
+                     });
             return services;
         }
 
         [PublicAPI]
-        public IServiceCollection AddAkkaSystem(AkkaConfig config) {
+        public IServiceCollection AddAkkaSystem(AkkaConfig config, params ActorRegistration[] registrations) {
             var system = config.GetSystem();
-            return services.AddAkkaSystem(system, config.ToHocon(system).Unwrap());
+            return services.AddAkkaSystem(system, config.ToHocon(system).Unwrap(), registrations);
         }
     }
 
---
+++ src/Akka/Impl/AkkaServices.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Frozen;
+using Akka.Actor;
+
+namespace RZ.Foundation.Akka.Impl;
+
+sealed class AkkaServices : IAKkaServices
+{
+    public FrozenDictionary<string, IActorRef> Actors = FrozenDictionary<string, IActorRef>.Empty;
+
+    public IActorRef? GetService(string name)
+        => Actors.GetValueOrDefault(name);
+}
